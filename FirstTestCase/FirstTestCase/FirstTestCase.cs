877fe9d8e62178ce7b50348657a60670c43fb0d4
duansuperman <duansupper@gmail.com>
2019-12-15 10:43:26 +0700

    create new first project

---
+++ FirstTestCase/FirstTestCase/FirstTestCase.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTestCase
+{
+    class FirstTestCase
+    {
+        static void Main(string[] args)
+        {
+            ChromeDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.demoqa.com");
+        }
+    }
+}
---
+++ FirstTestCase/FirstTestCase/NunitTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTestCase
+{
+    class NunitTest
+    {
+        ChromeDriver driver;
+        [SetUp]  //Start beginning
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+        }
+        [Test]  //Execute the test
+        public void OpenAppTest()
+        {
+            driver.Navigate().GoToUrl("http://www.demoqa.com");
+        }
+        [TearDown]  //Terminate the test
+        public void EndTest()
+        {
+            driver.Close();
+        }
+    }
+}
