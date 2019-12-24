6210be87f999964ec4848786f3e0060d5615a73a
duansuperman <duansupper@gmail.com>
2019-12-25 01:23:09 +0700

    Add read data fro json file

---
+++ Operations/IWebDriver/IWebDriver_Commands/Service/JsonDriver.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IWebDriver_Commands.Service
+{
+    class JsonDriver
+    {
+        public static JObject ReadDataFromJson(string pathToJson)
+        {
+            string filepath = pathToJson;
+            
+            using (StreamReader r = new StreamReader(filepath))
+            {
+
+                //Read file json
+
+                var json = r.ReadToEnd();
+
+                //convert json to jobject
+
+                var jobj = JObject.Parse(json);
+
+                return jobj;
+
+            }
+            
+        }
+        
+    }
+}
---
+++ Operations/IWebDriver/IWebDriver_Commands/TestSuites/less5_Read_Json_file.cs
@@ -0,0 +1,39 @@
+﻿using IWebDriver_Commands.Service;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IWebDriver_Commands.TestSuites
+{
+    class less5_Read_Json_file
+    {
+
+        [Test]
+        public void TestReadJson()
+        {
+            var jobj = JsonDriver.ReadDataFromJson("D:\\Khóa học selenimum C#\\Selenium c# - framework\\Operations\\IWebDriver\\IWebDriver_Commands\\TestData\\Text.json");
+
+            //Get value of key child
+
+            string t = jobj["Json"]["j2"].Value<String>();
+
+            Console.WriteLine(t);
+
+            //Get value of key parent
+
+            string k = jobj["PowerOff"].Value<String>();
+
+            Console.WriteLine(jobj["PowerOff"].Value<String>());
+
+            //Get value from array in json
+
+            Console.WriteLine(jobj["Array"][1]["a2"].Value<String>());
+
+        }
+    }
+}
