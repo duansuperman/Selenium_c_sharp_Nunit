917cb2d30e52707fdb5a1c8d266c3cbc18eb535f
duansuperman <duansupper@gmail.com>
2019-12-25 18:06:36 +0700

    Add execute javascript in selenium

---
+++ Operations/IWebDriver/IWebDriver_Commands/TestSuites/less6_CheckBox_Radio_Operations.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IWebDriver_Commands.TestSuites
+{
+    class less6_CheckBox_Radio_Operations
+    {
+        ChromeDriver driver;
+
+        [SetUp]
+
+        public void Intialize()
+        {
+            driver = new ChromeDriver();
+
+            //Navigate to practice page
+
+            driver.Navigate().GoToUrl("https://www.toolsqa.com/automation-practice-form/");
+        }
+
+        [Test]
+
+        public void Test()
+        {
+
+            //challenge 1 - select the deselected radio button (female)
+
+            driver.ExecuteScript("jQuery('#sex-1').click()");
+
+            //Challenge 2 - select the third radio button for category "years of Experience"
+
+            driver.ExecuteScript("jQuery('#exp-4').click()");
+
+            //Challenge 3 - select the checkbox "Automation Tester" for category "Profession"
+
+            driver.ExecuteScript("jQuery('#profession-1').click()");
+
+            //Challenge 4 - select checkbox "Selenium IDE" for category "Automation tool"
+
+            driver.ExecuteScript("jQuery('#tool-1').click()");
+
+           
+        }
+
+        [TearDown]
+
+        public void EndTest()
+        {
+            //driver.Quit(); //Close all windows opening 
+        }
+    }
+}
---
+++ Operations/IWebDriver/IWebDriver_Commands/TestSuites/less7_Execute_Javascript.cs
@@ -0,0 +1,71 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IWebDriver_Commands.TestSuites
+{
+    class less7_Execute_Javascript
+    {
+
+        ChromeDriver driver;
+
+        [SetUp]
+
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+            driver.Url = "https://www.c-sharpcorner.com/register";
+
+            //Scale full maximize screen
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+
+        public void Test()
+        {
+            
+            //Get element email input
+
+            IWebElement EnterEmail = driver.FindElement(By.Id("ctl00_ContentMain_TextBoxEmail"));
+
+
+            IJavaScriptExecutor jse = (IJavaScriptExecutor)driver;
+
+            //Set value for "Email" field
+
+            jse.ExecuteScript("document.getElementById('ctl00_ContentMain_TextBoxEmail').value='khajamoizuddin@gmail.com'");
+
+            //or
+
+            jse.ExecuteScript("arguments[0].value = 'duansupper@gmail.com'",EnterEmail);
+
+            //or
+
+            string mail = "abcxyz@gmail.com";
+            jse.ExecuteScript("arguments[0].value = arguments[1]", EnterEmail, mail);
+
+            //Get element button register
+
+            var registerButton = driver.FindElementById("csharLogin");
+
+            //Click on the register button
+
+            jse.ExecuteScript("arguments[0].click()", registerButton);
+
+
+        }
+
+        [TearDown]
+
+        public void EndTest()
+        {
+
+        }
+    }
+}
