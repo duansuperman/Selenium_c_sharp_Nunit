9e9e4e58f615ed5b281c8224035d5f0fb0e6163c
duansuperman <duansupper@gmail.com>
2020-02-10 11:51:09 +0700

    Handle alex, multi windows, implicit wait

---
+++ Operations/IWebDriver/IWebDriver_Commands/TestSuites/less11_Handle_Multi_Windows.cs
@@ -0,0 +1,87 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IWebDriver_Commands.TestSuites
+{
+    class less11_Handle_Multi_Windows
+    {
+
+        ChromeDriver driver;
+
+        string newBrowserWindow_Xpath = ".//button[@id=\"button1\"]";
+
+        [SetUp]
+
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+            driver.Url = "https://www.toolsqa.com/automation-practice-switch-windows/";
+        }
+
+        [Test]
+
+        public void Test()
+        {
+            //Print the parent window
+
+            string parentWindow = driver.CurrentWindowHandle;
+
+            Console.WriteLine("Parent window's handle -> " + parentWindow);
+
+            for(int i = 0; i < 3; i++)
+            {
+
+                IJavaScriptExecutor jse = (IJavaScriptExecutor) driver;
+
+                //Get element object of new browser window button
+
+                IWebElement newBrowserWindow_Element = driver.FindElementByXPath(newBrowserWindow_Xpath);
+
+                jse.ExecuteScript("arguments[0].click()", newBrowserWindow_Element);
+
+               
+            }
+
+            //Get all windows of both one parent and many childs
+
+            List<String> stringWindow = driver.WindowHandles.ToList();
+
+            foreach(string item in stringWindow)
+            {
+
+                //print each and very window in the list of string windows
+
+                Console.WriteLine(item);
+
+                //Switch to child window which is fetched from the focused window
+
+                driver.SwitchTo().Window(item);
+
+                //Navigates to the goodle chrome url
+
+                driver.Navigate().GoToUrl("https://www.google.com/");
+
+            }
+
+           
+            //Must assign to focus another window to continue using the next command
+
+            driver.SwitchTo().Window(stringWindow.ElementAt(3));
+            
+            driver.Close();
+        }
+
+        [TearDown]
+
+        public void End()
+        {
+
+        }
+    }
+}
---
+++ Operations/IWebDriver/IWebDriver_Commands/TestSuites/less13_Implicit_Wait.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IWebDriver_Commands.TestSuites
+{
+    class less13_Implicit_Wait
+    {
+
+        ChromeDriver driver;
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+            driver.Url = "https://www.toolsqa.com/automation-practice-switch-windows/";
+        }
+
+        [Test]
+        public void Test()
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
+
+            IWebElement visible_Element = driver.FindElementByXPath(".//button[@id='invisibility']");
+
+            bool check = visible_Element.Displayed;
+
+            Console.WriteLine(visible_Element.Text);
+
+            if (check == true)
+            {
+                Console.WriteLine("Just visible");
+            }
+        }
+
+        [TearDown]
+        public void End()
+        {
+
+        }
+    }
+}
